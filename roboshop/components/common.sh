ec091b1c8114ff75c678292eb6d7b9b5aabc7e7d
Kartheek39 <bablutech39@gmail.com>
2021-09-10 14:31:02 +0000

    code update

---
+++ roboshop/components/common.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+status_check() {
+  if [ $1 -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi
+}
+print(){
+  echo -n -e "$1 \t- "
+}
+if [$UID -ne 0];then
+    echo -e "\e[1;33muser should be as root user"
