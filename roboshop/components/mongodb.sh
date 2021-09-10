b5848333bbe1fcedaddb28965c8b15b4c1ff746c
Kartheek39 <bablutech39@gmail.com>
2021-09-10 13:17:14 +0000

    code commit updated

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+status_check() {
+  if [ $1 -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"]
+else
+  echo -e "\e[31mFAILURE\e[0m"]
+  exit 2
+fi
+}
+
 echo "setting mongodb repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -6,63 +15,27 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
 echo "installing mongodb"
 yum install -y mongodb-org &>>/tmp/log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
 echo "starting mongodb"
 systemctl enable mongod
 systemctl start mongod
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
-
+status_check$?
 echo "configuring mongodb"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 sysemctl restart mongod
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
 echo "downloading schema "
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
 echo "extracting archive schema file"
 unzip -o mongodb.zip &>>/tmp/log
 cd mongodb-main
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
 echo "loading schema to mongodb"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"]
-else
-  echo -e "\e[31mFAILURE\e[0m"]
-  exit 2
-fi
+status_check$?
