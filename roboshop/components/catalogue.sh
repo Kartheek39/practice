67d272625dd5af0afba78e1943ad5696b46f8806
Kartheek39 <bablutech39@gmail.com>
2021-09-10 16:40:28 +0000

    updating data

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 source components/common.sh
+print "installing nodejs"
+yum install nodejs make gcc-c++ -y
+status_check $?
+print "adding user"
+useradd roboshop
+status_check $?
+print "downloading catalogue file"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+status_check $?
+print "redirecting to roboshop directory"
+cd /home/roboshop
+status_check
+print "extracting catalogue archieved file"
+unzip -o /tmp/catalogue.zip
+mv catalogue-main catalogue
+status_check $?
+print "installing npm"
+cd /home/roboshop/catalogue
+npm install
+status_check $?
+print "starting catalogue services"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+status_check $?
