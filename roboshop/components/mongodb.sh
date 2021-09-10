196c5c4e18452ff00d24e67d6b1152ec0c7fcbb7
Kartheek39 <bablutech39@gmail.com>
2021-09-10 13:25:19 +0000

    code update

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -15,27 +15,27 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-status_check$?
+status_check $?
 echo "installing mongodb"
 yum install -y mongodb-org &>>/tmp/log
-status_check$?
+status_check $?
 echo "starting mongodb"
 systemctl enable mongod
 systemctl start mongod
-status_check$?
+status_check $?
 echo "configuring mongodb"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 sysemctl restart mongod
-status_check$?
+status_check $?
 echo "downloading schema "
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
-status_check$?
+status_check $?
 echo "extracting archive schema file"
 unzip -o mongodb.zip &>>/tmp/log
 cd mongodb-main
-status_check$?
+status_check $?
 echo "loading schema to mongodb"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
-status_check$?
+status_check $?
