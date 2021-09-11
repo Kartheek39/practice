a5abaca4292ce602c9cd5bc0e1816064e931aa5e
Kartheek39 <bablutech39@gmail.com>
2021-09-11 13:19:07 +0000

    updating catalogue script

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,15 +3,15 @@ source components/common.sh
 print "installing utils and download repo"
 yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 status_check $?
-print "settup redis repo"
+print "settup redis repo\t\t"
 yum-config-manager --enable remi &>>$LOG
 status_check $?
-print "installing redis"
+print "installing redis\t\t"
 yum install redis -y &>>$LOG
 status_check $?
-print "configuring redis ip address"
+print "configuring redis ip address\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 status_check $?
-print "Starting redis"
+print "Starting redis\t\t\t"
 systemctl enable redis && systemctl start redis &>>$LOG
 status_check $?
