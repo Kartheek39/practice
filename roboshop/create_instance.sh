ad417103dc5d243e1d6c9eea96f61dd323a2efa7
Kartheek39 <bablutech39@gmail.com>
2021-09-12 17:18:26 +0000

    create instance push

--- roboshop/create_instance.sh
+++ roboshop/create_instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LID=lt-05397bf2df1fb116f
 LVER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq .Instances[].InstanceId 
+aws ec2 run-instances --launch-template LaunchTemplateId = $LID,Version = $LVER | jq .Instances[].InstanceId 
  
