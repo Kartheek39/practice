6e9e1948eb4b457602ed98b1d268ac4826a91bc2
Kartheek39 <bablutech39@gmail.com>
2021-09-13 11:05:45 +0000

    create instance push

--- roboshop/create_instance.sh
+++ roboshop/create_instance.sh
@@ -2,9 +2,9 @@
 LID=lt-05397bf2df1fb116f
 LVER=2
 INSTANCE_NAME=$1
-if [ -z "${INSTANCE_NAME"} ];then
-    echo "input is missing"
-    exit 1
+if [ -z "${INSTANCE_NAME"} ]; then
+  echo "input is missing"
+  exit 1 
 fi
-InstanceId=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,
-Tags=[{Key=Name,Value=$INSTANCE_NAME}" s "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}"  | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+Ip=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,
+Tags=[{Key=Name,Value=$INSTANCE_NAME}" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
