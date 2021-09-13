755a079c957edbca83dc4175b84ee719f5ad321f
Kartheek39 <bablutech39@gmail.com>
2021-09-13 11:11:48 +0000

    create instance push

--- roboshop/create_instance.sh
+++ roboshop/create_instance.sh
@@ -2,8 +2,8 @@
 LID=lt-05397bf2df1fb116f
 LVER=2
 INSTANCE_NAME=$1
-if [ -z "${INSTANCE_NAME"} ]; then
-  echo "input is missing"
+if [ -z "${INSTANCE_NAME}" ]; then
+  echo Input is missing
   exit 1 
 fi
 Ip=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,
