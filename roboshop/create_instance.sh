490b3e52d9b0bd22ce3fdfe0896cada44fdee621
Kartheek39 <bablutech39@gmail.com>
2021-09-13 11:56:47 +0000

    create instance push

--- roboshop/create_instance.sh
+++ roboshop/create_instance.sh
@@ -7,5 +7,5 @@ if [ -z "${INSTANCE_NAME}" ]; then
   exit 1 
 fi
 
-IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER '--tag-specifications' "ResourceType=spot-instances-request,
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId =$LID,Version =$LVER --tag-specifications "ResourceType=spot-instances-request,
 Tags=[{Key=Name,Value=$INSTANCE_NAME}" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
