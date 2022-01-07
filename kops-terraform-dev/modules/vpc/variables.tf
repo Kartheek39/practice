2c52f9e8e874f58e9e15924c15501ca464c35af7
Kartheek39 <bablutech39@gmail.com>
2022-01-07 06:43:22 +0530

    code update

--- kops-terraform-dev/modules/vpc/variables.tf
+++ kops-terraform-dev/modules/vpc/variables.tf
@@ -4,7 +4,7 @@ variable "vpc" {
     public_subnets = list(string)
     private_subnets = list(string)
     rds_subnets     = list(string)
-    elasticache_subnets = list(string)
+#    elasticache_subnets = list(string)
     dns_hostnames = bool
     dns_support   = bool
     tenancy       = string
@@ -14,9 +14,9 @@ variable "vpc" {
   default = {
     cidr            = "10.2.0.0/16"
     public_subnets  = ["10.2.0.0/24","10.2.1.0/24"]
-    private_subnets = ["10.2.2.0/24","10.2.3.0/24","10.2.4.0/24"]
+    private_subnets = ["10.2.2.0/24","10.2.3.0/24","10.2.4.0/24","10.2.5.0/24"]
     rds_subnets     = []
-    elasticache_subnets = []
+#    elasticache_subnets = []
     dns_hostnames   = true
     dns_support     = true
     tenancy         = "default"
