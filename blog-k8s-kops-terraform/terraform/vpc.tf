976b11509a680c6d852dcbb881bd3079411aef53
Kartheek39 <Kartheek39>
2022-01-13 12:07:08 +0530

    code update

--- blog-k8s-kops-terraform/terraform/vpc.tf
+++ blog-k8s-kops-terraform/terraform/vpc.tf
@@ -11,8 +11,8 @@ module "dev_vpc" {
   tags = {
     // This is so kops knows that the VPC resources can be used for k8s
 #    "kubernetes.io/cluster/${local.kubernetes_cluster_name}" = "shared"
-    "terraform"                                              = true
-    "environment"                                            = "${local.environment}"
+    terraform                                              = "true"
+    environment                                            = "${local.environment}"
   }
 
   // Tags required by k8s to launch services on the right subnets
