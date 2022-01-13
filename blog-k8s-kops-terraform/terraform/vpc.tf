418131af68a3de652983cf010633e18e4f8e9ab4
Kartheek39 <Kartheek39>
2022-01-13 12:20:49 +0530

    code update

--- blog-k8s-kops-terraform/terraform/vpc.tf
+++ blog-k8s-kops-terraform/terraform/vpc.tf
@@ -1,26 +1,18 @@
-module "dev_vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "1.46.0"
-  name               = "${local.vpc_name}"
-  cidr               = "172.31.0.0/16"
-  azs                = ["us-east-1a", "us-east-1b"]
-  private_subnets    = ["172.31.1.0/24", "172.31.2.0/24", "172.31.3.0/24"]
-  public_subnets     = ["172.31.4.0/24", "172.31.5.0/24", "172.31.6.0/24"]
-  enable_nat_gateway = true
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
 
-  tags = {
-    // This is so kops knows that the VPC resources can be used for k8s
-#    "kubernetes.io/cluster/${local.kubernetes_cluster_name}" = "shared"
-    terraform                                              = "true"
-    environment                                            = "${local.environment}"
+    name = "my-vpc"
+    cidr = "10.0.0.0/16"
+
+    azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+    enable_nat_gateway = true
+    enable_vpn_gateway = true
+
+    tags = {
+      Terraform = "true"
+      Environment = "dev"
+    }
   }
-
-  // Tags required by k8s to launch services on the right subnets
-#  private_subnet_tags = {
-#    "kubernetes.io/role/internal-elb" = true
-#  }
-#
-#  public_subnet_tags = {
-#    "kubernetes.io/role/elb" = true
-#  }
-}
