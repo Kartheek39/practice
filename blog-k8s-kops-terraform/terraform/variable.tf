654c8423f8492c934d2bf86ba119d81434fe70d5
Kartheek39 <Kartheek39>
2022-01-13 12:42:49 +0530

    code update

---
+++ blog-k8s-kops-terraform/terraform/variable.tf
@@ -0,0 +1,4 @@
+variable "vpc_cidr_block" {}
+variable "avail_zone" {}
+variable "private_cidr_block" {}
+variable "public_cidr_block" {}
--- blog-k8s-kops-terraform/terraform/vpc.tf
+++ blog-k8s-kops-terraform/terraform/vpc.tf
@@ -2,11 +2,11 @@ module "dev_vpc" {
     source = "terraform-aws-modules/vpc/aws"
 
     name = "my-vpc"
-    cidr = "10.0.0.0/16"
+    cidr = var.vpc_cidr_block
 
-    azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    azs             = [var.avail_zone]
+    private_subnets = [var.private_cidr_block]
+    public_subnets  = [var.public_cidr_block]
 
     enable_nat_gateway = true
     enable_vpn_gateway = true
