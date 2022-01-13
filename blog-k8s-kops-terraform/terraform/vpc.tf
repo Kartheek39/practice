4fdcbf31c0e525e23d1e7498d0ae71b5a885e4ec
Kartheek39 <Kartheek39>
2022-01-13 13:35:18 +0530

    code update

--- blog-k8s-kops-terraform/terraform/vpc.tf
+++ blog-k8s-kops-terraform/terraform/vpc.tf
@@ -10,17 +10,17 @@ module "dev_vpc" {
 
   tags = {
     // This is so kops knows that the VPC resources can be used for k8s
-    "kubernetes.io/cluster/${local.kubernetes_cluster_name}" = "shared"
+#    "kubernetes.io/cluster/${local.kubernetes_cluster_name}" = "shared"
     "terraform"                                              = true
     "environment"                                            = "${local.environment}"
   }
 
   // Tags required by k8s to launch services on the right subnets
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = true
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = true
-  }
-}
+#  private_subnet_tags = {
+#    "kubernetes.io/role/internal-elb" = true
+#  }
+#
+#  public_subnet_tags = {
+#    "kubernetes.io/role/elb" = true
+#  }
+#}
