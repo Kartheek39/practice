1f06a1c312ef8882a042e93bdadf2401aa6cb2af
Kartheek39 <Kartheek39>
2022-01-20 23:14:52 +0530

    commit

--- blog-k8s-kops-terraform/terraform/s3_buckets.tf
+++
@@ -1,7 +0,0 @@
-// S3 bucket to store kops state.
-resource "aws_s3_bucket" "kops_state" {
-  bucket        = "${local.kops_state_bucket_name}"
-  acl           = "private"
-  force_destroy = true
-  tags          = "${merge(local.tags)}"
-}
